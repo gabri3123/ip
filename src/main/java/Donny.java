e0a962ce70c8c0dce5e2bc48f43df6a07d8e6220
gabri3123 <E1398006@u.nus.edu>
2026-02-01 16:12:37 +0800

    level 0

---
+++ src/main/java/Donny.java
@@ -0,0 +1,12 @@
+public class Donny {
+    public static void main(String[] args) {
+        String logo = " ____   ___   _   _   _   _   _  _ \n"
+                + "|  _ \\ / _ \\ | \\ | | | \\ | | | \\/ |\n"
+                + "| | | | | | ||  \\| | |  \\| | \\   /\n"
+                + "| |_| | |_| || |\\  | | |\\  |  | | \n"
+                + "|____/ \\___/ |_| \\_| |_| \\_|  |_|  \n";
+        String line = "_________________________________\n";
+        System.out.println("Hello! I'm \n" + logo + "\n" + "What can I do for you? \n");
+        System.out.println(line + "See you again, bye!\n" + line);
+    }
+}
