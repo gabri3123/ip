f9403e112887023571cd008ec1206b0fa158d57a
gabri3123 <E1398006@u.nus.edu>
2026-02-01 23:30:54 +0800

    level-1

--- src/main/java/Donny.java
+++ src/main/java/Donny.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Donny {
     public static void main(String[] args) {
         String logo = " ____   ___   _   _   _   _   _  _ \n"
@@ -5,8 +7,19 @@ public class Donny {
                 + "| | | | | | ||  \\| | |  \\| | \\   /\n"
                 + "| |_| | |_| || |\\  | | |\\  |  | | \n"
                 + "|____/ \\___/ |_| \\_| |_| \\_|  |_|  \n";
-        String line = "_________________________________\n";
+        String longline = "_________________________________\n";
         System.out.println("Hello! I'm \n" + logo + "\n" + "What can I do for you? \n");
-        System.out.println(line + "See you again, bye!\n" + line);
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            line = in.nextLine();
+            System.out.println(longline + line + "\n");
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println(longline + "See you again, bye!\n" + longline);
+                break;
+            }
+        }
     }
 }
