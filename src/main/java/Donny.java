42b82c0c1ae1c7ecdc5694d2a3cf4d0c1d8c1ea0
gabri3123 <E1398006@u.nus.edu>
2026-02-08 01:36:09 +0800

    level-3

--- src/main/java/Donny.java
+++ src/main/java/Donny.java
@@ -12,28 +12,60 @@ public class Donny {
 
         String line;
         Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] tasks = new Task[100];
         int size = 0;
+
+        boolean[] done = new boolean[100];
 
         while (true) {
             line = in.nextLine();
 
             if (line.equalsIgnoreCase("list")) {
                 System.out.println(longline);
+                System.out.println("Here are tasks in your list\n");
+
                 for (int i = 0; i < size; i++) {
-                    System.out.println((i + 1) + "." + list[i] + "\n");
+                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
+
                 System.out.println(longline);
                 continue;
             }
+
+            if (line.toLowerCase().startsWith("mark")){
+
+                String[]parts = line.split("\\s+");
+                int index = Integer.parseInt(parts[1]) - 1;
+
+                tasks[index].markAsDone();
+
+                System.out.println(longline);
+                System.out.println(" Nice! I've marked this task as done:\n");
+                System.out.println("  [X]  " + tasks[index].getDescription());
+                continue;
+            }
+
+            if (line.toLowerCase().startsWith("unmark")) {
+
+                String[] parts = line.split("\\s+");
+                int index = Integer.parseInt(parts[1]) - 1;
+
+                tasks[index].markAsNotDone();
+
+                System.out.println(longline);
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   [ ] " + tasks[index].getDescription());
+                continue;
+            }
+
 
             if (line.equalsIgnoreCase("bye")) {
                 System.out.println(longline + "See you again, bye!\n" + longline);
                 break;
             }
 
-            list[size] = line;
-            size ++;
+            tasks[size] = new Task(line);
+            size++;
             System.out.println(longline + "added:" + line + "\n");
         }
     }
